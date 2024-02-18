227ec1aa6a8871201a91e429a763681fdf4fc4f0
hicmtrex <hichembouallegue@gmail.com>
2024-02-18 16:07:04 +0100

    add lazy loading

--- src/screens/products/product-details.js
+++ src/screens/products/product-details.js
@@ -6,28 +6,28 @@ import {
   TouchableOpacity,
   Button,
   TextInput,
-} from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { useNavigation, useRoute } from '@react-navigation/native';
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import {
   ArrowLeftIcon,
   ChevronDownIcon,
   ChevronUpIcon,
-} from 'react-native-heroicons/solid';
-import * as Animatable from 'react-native-animatable';
-import Rating from '../../components/UI/rating';
-import { dateFormater } from '../../utils/formater';
-import { useDispatch, useSelector } from 'react-redux';
-import Products from '../../components/products/products';
-import CartIcon from '../../components/UI/cart-icon';
-import DefaultLayout from '../../components/layouts/default-layout';
-import { addToCart } from '../../store/cart/cart';
-import { Picker } from '@react-native-picker/picker';
-import { useToast } from 'react-native-toast-notifications';
-import { setError } from '../../utils/error';
-import Loader from '../../components/UI/loader';
-import { createComment } from '../../store/products/comment';
-import { getProductById } from '../../store/products/product-details';
+} from "react-native-heroicons/solid";
+import * as Animatable from "react-native-animatable";
+import Rating from "../../components/UI/rating";
+import { dateFormater } from "../../utils/formater";
+import { useDispatch, useSelector } from "react-redux";
+import Products from "../../components/products/products";
+import CartIcon from "../../components/UI/cart-icon";
+import DefaultLayout from "../../components/layouts/default-layout";
+import { addToCart } from "../../store/cart/cart";
+import { Picker } from "@react-native-picker/picker";
+import { useToast } from "react-native-toast-notifications";
+import { setError } from "../../utils/error";
+import Loader from "../../components/UI/loader";
+import { createComment } from "../../store/products/comment";
+import { getProductById } from "../../store/products/product-details";
 
 const ProductDetails = () => {
   const route = useRoute();
@@ -37,10 +37,11 @@ const ProductDetails = () => {
   const [show, setShow] = useState(false);
   const [showReviews, setShowReviews] = useState(false);
   const [showC, setShowC] = useState(false);
-  const [rating, setRating] = useState('⭐');
-  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState("⭐");
+  const [comment, setComment] = useState("");
   const { products } = useSelector((state) => state.products);
   const { isAuth } = useSelector((state) => state.login);
+  const [refetch, setRefetch] = useState(false);
   const { success, loading, error, refresh } = useSelector(
     (state) => state.comment
   );
@@ -52,7 +53,7 @@ const ProductDetails = () => {
   // add to basket
   const onBuy = () => {
     dispatch(addToCart(product));
-    navigation.navigate('CartBasket');
+    navigation.navigate("CartBasket");
   };
 
   //Add Comment
@@ -61,73 +62,74 @@ const ProductDetails = () => {
     dispatch(createComment(review));
 
     if (success) {
-      setComment('');
+      setRefetch(!refetch);
+      setComment("");
       setShowC(false);
-      toast.show('thanks for your feedback', {
-        type: 'success',
+      toast.show("thanks for your feedback", {
+        type: "success",
       });
     } else if (error) {
-      setComment('');
+      setComment("");
       setShowC(false);
       toast.show(setError(error), {
-        type: 'danger',
+        type: "danger",
       });
     }
   };
 
   useEffect(() => {
     dispatch(getProductById(id));
-  }, [id, dispatch, refresh]);
+  }, [id, dispatch, refetch]);
 
   if (loadingProduct || !product) return <Loader />;
   return (
     <DefaultLayout>
-      <View className='bg-black flex-row items-center justify-between px-2 text-white space-x-2  h-16'>
-        <View className='flex-row  items-center'>
-          <ArrowLeftIcon color={'white'} onPress={navigation.goBack} />
-          <Text className='text-white text-xl ml-5'>Details</Text>
+      <View className="bg-black flex-row items-center justify-between px-2 text-white space-x-2  h-16">
+        <View className="flex-row  items-center">
+          <ArrowLeftIcon color={"white"} onPress={navigation.goBack} />
+          <Text className="text-white text-xl ml-5">Details</Text>
         </View>
 
         <CartIcon />
       </View>
       <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
-        <View className='bg-white py-3'>
+        <View className="bg-white py-3">
           <Image
             source={{ uri: product?.image }}
-            className='w-full  h-60'
-            resizeMode='contain'
+            className="w-full  h-60"
+            resizeMode="contain"
           />
         </View>
-        <View className='pt-3  space-y-2'>
-          <Text className=' font-bold px-3'>{product?.name}</Text>
-          <View className='flex-row items-center px-3'>
-            <Text className='text-xs text-gray-500 flex-1'>
+        <View className="pt-3  space-y-2">
+          <Text className=" font-bold px-3">{product?.name}</Text>
+          <View className="flex-row items-center px-3">
+            <Text className="text-xs text-gray-500 flex-1">
               Brand: {product?.brand} | Category: {product?.category}
             </Text>
-            <Text className='text-xl font-bold'>${product?.price}</Text>
+            <Text className="text-xl font-bold">${product?.price}</Text>
           </View>
           <View>
-            <Text className='bg-gray-200 px-3 py-3 font-bold text-gray-500'>
+            <Text className="bg-gray-200 px-3 py-3 font-bold text-gray-500">
               PRODUCT DETAILS
             </Text>
-            <View className='flex-row items-center justify-between border-b border-gray-300 px-3 h-10 bg-[#f6f9ff] shadow mx-2'>
-              <Text className='font-bold '>Description:</Text>
+            <View className="flex-row items-center justify-between border-b border-gray-300 px-3 h-10 bg-[#f6f9ff] shadow mx-2">
+              <Text className="font-bold ">Description:</Text>
               {!show ? (
                 <TouchableOpacity onPress={() => setShow(true)}>
-                  <ChevronDownIcon color={'black'} />
+                  <ChevronDownIcon color={"black"} />
                 </TouchableOpacity>
               ) : (
                 <TouchableOpacity onPress={() => setShow(false)}>
-                  <ChevronUpIcon color={'black'} />
+                  <ChevronUpIcon color={"black"} />
                 </TouchableOpacity>
               )}
             </View>
 
-            <View className='bg-[#f6f9ff] shadow mx-2 px-3'>
+            <View className="bg-[#f6f9ff] shadow mx-2 px-3">
               {show && (
                 <Animatable.Text
-                  className='text-xs text-gray-500 my-2'
-                  animation={'fadeIn'}
+                  className="text-xs text-gray-500 my-2"
+                  animation={"fadeIn"}
                   iterationCount={0.5}
                 >
                   {product.description}
@@ -136,36 +138,36 @@ const ProductDetails = () => {
             </View>
           </View>
           <View>
-            <View className='flex-row items-center justify-between border-b border-gray-300 px-3 h-10 bg-[#f6f9ff] shadow mx-2'>
-              <Text className='font-bold '>Reviews:</Text>
+            <View className="flex-row items-center justify-between border-b border-gray-300 px-3 h-10 bg-[#f6f9ff] shadow mx-2">
+              <Text className="font-bold ">Reviews:</Text>
               {!showReviews ? (
                 <TouchableOpacity onPress={() => setShowReviews(true)}>
-                  <ChevronDownIcon color={'black'} />
+                  <ChevronDownIcon color={"black"} />
                 </TouchableOpacity>
               ) : (
                 <TouchableOpacity onPress={() => setShowReviews(false)}>
-                  <ChevronUpIcon color={'black'} />
+                  <ChevronUpIcon color={"black"} />
                 </TouchableOpacity>
               )}
             </View>
 
-            <View className='bg-[#f6f9ff] shadow mx-2 px-3'>
+            <View className="bg-[#f6f9ff] shadow mx-2 px-3">
               {showReviews && (
-                <Animatable.View animation={'fadeIn'} iterationCount={0.5}>
+                <Animatable.View animation={"fadeIn"} iterationCount={0.5}>
                   {product.reviews.map((review) => (
                     <View
                       key={review._id}
-                      className=' h-16 border-b border-gray-300'
+                      className=" h-16 border-b border-gray-300"
                     >
-                      <View className='flex-row justify-between items-center py-2'>
+                      <View className="flex-row justify-between items-center py-2">
                         <Rating value={review.rating} />
-                        <Text className='text-xs'>By {review?.name}</Text>
+                        <Text className="text-xs">By {review?.name}</Text>
                       </View>
-                      <View className='flex-row'>
-                        <Text className='text-xs flex-1'>
+                      <View className="flex-row">
+                        <Text className="text-xs flex-1">
                           {review?.comment}
                         </Text>
-                        <Text className='text-xs'>
+                        <Text className="text-xs">
                           {dateFormater(review?.createdAt)}
                         </Text>
                       </View>
@@ -174,15 +176,15 @@ const ProductDetails = () => {
                 </Animatable.View>
               )}
               {isAuth && (
-                <View className='flex-row items-center justify-between border-b border-gray-300  h-10 bg-[#f6f9ff] shadow '>
-                  <Text className='font-bold '>Comment:</Text>
+                <View className="flex-row items-center justify-between border-b border-gray-300  h-10 bg-[#f6f9ff] shadow ">
+                  <Text className="font-bold ">Comment:</Text>
                   {!showC ? (
                     <TouchableOpacity onPress={() => setShowC(true)}>
-                      <ChevronDownIcon color={'black'} />
+                      <ChevronDownIcon color={"black"} />
                     </TouchableOpacity>
                   ) : (
                     <TouchableOpacity onPress={() => setShowC(false)}>
-                      <ChevronUpIcon color={'black'} />
+                      <ChevronUpIcon color={"black"} />
                     </TouchableOpacity>
                   )}
                 </View>
@@ -193,43 +195,43 @@ const ProductDetails = () => {
                 <>
                   {showC && isAuth ? (
                     <Animatable.View
-                      animation={'fadeIn'}
+                      animation={"fadeIn"}
                       iterationCount={0.5}
-                      className='p-2'
+                      className="p-2"
                     >
-                      <View className='border border-gray-300'>
+                      <View className="border border-gray-300">
                         <Picker
                           selectedValue={rating}
                           onValueChange={(itemValue, itemIndex) =>
                             setRating(itemValue)
                           }
                         >
-                          <Picker.Item label={'⭐'} value={1} />
-                          <Picker.Item label={'⭐⭐'} value={2} />
-                          <Picker.Item label={'⭐⭐⭐'} value={3} />
-                          <Picker.Item label={'⭐⭐⭐⭐'} value={4} />
-                          <Picker.Item label={'⭐⭐⭐⭐⭐'} value={5} />
+                          <Picker.Item label={"⭐"} value={1} />
+                          <Picker.Item label={"⭐⭐"} value={2} />
+                          <Picker.Item label={"⭐⭐⭐"} value={3} />
+                          <Picker.Item label={"⭐⭐⭐⭐"} value={4} />
+                          <Picker.Item label={"⭐⭐⭐⭐⭐"} value={5} />
                         </Picker>
                       </View>
-                      <View className='p-2'>
+                      <View className="p-2">
                         <TextInput
                           multiline
                           value={comment}
                           onChangeText={(text) => setComment(text)}
                           numberOfLines={5}
-                          placeholder='Your Message'
-                          className='px-2 bg-gray-200 rounded-md '
+                          placeholder="Your Message"
+                          className="px-2 bg-gray-200 rounded-md "
                         />
                       </View>
-                      <View className='w-[50%] self-center rounded-lg'>
+                      <View className="w-[50%] self-center rounded-lg">
                         {loading ? (
                           <Loader />
                         ) : (
                           <TouchableOpacity
                             onPress={onSubmit}
-                            className='bg-gray-900 rounded p-2 '
+                            className="bg-gray-900 rounded p-2 "
                           >
-                            <Text className='text-center text-white '>
+                            <Text className="text-center text-white ">
                               Submit
                             </Text>
                           </TouchableOpacity>
@@ -239,16 +241,16 @@ const ProductDetails = () => {
                   ) : null}
                 </>
               )}
-              <View className='my-2'>
+              <View className="my-2">
                 <Button
-                  title='Buy Now'
-                  color={'#e03a3c'}
-                  className='w-100'
+                  title="Buy Now"
+                  color={"#e03a3c"}
+                  className="w-100"
                   onPress={onBuy}
                 />
               </View>
             </View>
-            <Text className='bg-gray-200 px-3 py-3 font-bold text-gray-500'>
+            <Text className="bg-gray-200 px-3 py-3 font-bold text-gray-500">
               SEMILAR PRODUCTS
             </Text>
             <Products products={products} />
--- src/screens/products/products-screen.js
+++ src/screens/products/products-screen.js
@@ -1,24 +1,39 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import React, { useEffect, useState } from 'react';
-import DefaultLayout from '../../components/layouts/default-layout';
-import SearchBox from '../../components/search-box';
-import { useDispatch, useSelector } from 'react-redux';
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import React, { useEffect, useState } from "react";
+import DefaultLayout from "../../components/layouts/default-layout";
+import SearchBox from "../../components/search-box";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getFilterProducts,
   setBrand,
   setCategory,
-} from '../../store/products/filter-products';
-import SingleProductCard from '../../components/products/single-productcard';
-import Loader from '../../components/UI/loader';
+} from "../../store/products/filter-products";
+import SingleProductCard from "../../components/products/single-productcard";
+import Loader from "../../components/UI/loader";
+import useDebounce from "../../hooks/useDebounce";
+import { useCallback } from "react";
+import SkeletonProductCard from "../../components/UI/product-skeleton";
 
 const ProductsScreen = () => {
   const dispatch = useDispatch();
+  const { filterProducts } = useSelector((state) => state);
   const { products, loading, categories, brands, brand, category, pages } =
-    useSelector((state) => state.filterProducts);
-  const [search, setSearch] = useState('');
+    filterProducts;
+
+  const [search, setSearch] = useState("");
+  const { debouncedValue } = useDebounce(search, 300);
   const [curPage, setCurPage] = useState(1);
-  const { success } = useSelector((state) => state.comment);
+
+  const handleCategoryChange = useCallback(
+    (itemValue, itemIndex) => dispatch(setCategory(itemValue)),
+    [dispatch]
+  );
+
+  const handleBrandChange = useCallback(
+    (itemValue, itemIndex) => dispatch(setBrand(itemValue)),
+    [dispatch]
+  );
 
   useEffect(() => {
     dispatch(
@@ -26,45 +41,40 @@ const ProductsScreen = () => {
         n: curPage,
         b: brand,
         c: category,
-        q: search,
+        q: debouncedValue,
       })
     );
-  }, [dispatch, search, category, curPage, brand, success]);
-
+  }, [dispatch, search, category, curPage, brand]);
   return (
     <DefaultLayout>
-      <View className='bg-gray-900 pt-2'>
+      <View className="bg-gray-900 pt-2">
         <SearchBox setSearch={setSearch} />
       </View>
-      <View className='bg-gray-900 flex-row border-t border-gray-100'>
-        <View className='w-[50%]'>
+      <View className="bg-gray-900 flex-row border-t border-gray-100">
+        <View className="w-[50%]">
           <Picker
             style={{
-              color: '#fff',
+              color: "#fff",
             }}
-            dropdownIconColor='#fff'
+            dropdownIconColor="#fff"
             selectedValue={category}
-            onValueChange={(itemValue, itemIndex) =>
-              dispatch(setCategory(itemValue))
-            }
+            onValueChange={handleCategoryChange}
           >
-            <Picker.Item label='All Categories' value={''} />
+            <Picker.Item label="All Categories" value={""} />
             {categories.map((c) => (
               <Picker.Item label={c} value={c} key={c} />
             ))}
           </Picker>
         </View>
-        <View className='w-[50%] border-l border-gray-100 '>
+        <View className="w-[50%] border-l border-gray-100 ">
           <Picker
-            dropdownIconColor='#fff'
-            style={{ color: '#fff' }}
-            itemStyle={{ color: '#fff' }}
+            dropdownIconColor="#fff"
+            style={{ color: "#fff" }}
+            itemStyle={{ color: "#fff" }}
             selectedValue={brand}
-            onValueChange={(itemValue, itemIndex) =>
-              dispatch(setBrand(itemValue))
-            }
+            onValueChange={handleBrandChange}
           >
-            <Picker.Item label='All Brands' value={''} />
+            <Picker.Item label="All Brands" value={""} />
             {brands.map((c) => (
               <Picker.Item label={c} value={c} key={c} />
             ))}
@@ -73,7 +83,13 @@ const ProductsScreen = () => {
       </View>
       {/* Products List */}
       {loading ? (
-        <Loader />
+        <>
+          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+            {[...Array(9).keys()].map((index) => (
+              <SkeletonProductCard key={index} />
+            ))}
+          </View>
+        </>
       ) : (
         <ScrollView contentContainerStyle={{ paddingBottom: 150 }}>
           {/* Card */}
@@ -81,12 +97,13 @@ const ProductsScreen = () => {
             <SingleProductCard key={product._id} product={product} />
           ))}
           {pages > 1 && (
-            <View className='flex-row px-2'>
+            <View className="flex-row px-2">
               {[...Array(pages).keys()].map((x) => (
                 <TouchableOpacity key={x} onPress={() => setCurPage(x + 1)}>
                   <Text
+                    key={`page-${x}`}
                     className={`mr-2 p-1 w-7 h-7 text-center text-white rounded-full ${
-                      curPage === x + 1 ? 'bg-red-600 ' : 'bg-gray-300'
+                      curPage === x + 1 ? "bg-red-600 " : "bg-gray-300"
                     }`}
                   >
                     {x + 1}
